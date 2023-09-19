2c0b71db7a10d58f4877f5ed07477ffc0c3e3044
Dilek1979 <dilekatilla79@outlook.com>
2023-09-19 18:16:52 +0200

    projeye devam

--- src/Main/dilek.java
+++ src/Main/dilek.java
@@ -5,20 +5,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class dilek extends BaseDriver {
+public class Dilek extends BaseDriver {
+
     @Test
     public void test1() {
+
         driver.get("https://demo.nopcommerce.com/");
-        WebElement login = driver.findElement(By.cssSelector("a[class='ico-login']"));
+        WebElement login=driver.findElement(By.cssSelector("a[class='ico-login]"));
         login.click();
-        WebElement email = driver.findElement(By.cssSelector("input[class='email']"));
-        email.sendKeys("dilekatilla79@outlook.com");
-        WebElement password = driver.findElement(By.cssSelector("input[class='password'"));
+        WebElement email= driver.findElement(By.cssSelector("input[class=email]"));
+        email.sendKeys("dilekatilla@outlook.com");
+        WebElement password= driver.findElement(By.cssSelector("input[class='password]"));
         password.sendKeys("ahmet2303");
-        WebElement remember = driver.findElement(By.cssSelector("input[id='RememberMe']"));
-        remember.click();
-        WebElement login2 = driver.findElement(By.cssSelector("button[type='submit']"));
-        login2.click();
 
 
     }
