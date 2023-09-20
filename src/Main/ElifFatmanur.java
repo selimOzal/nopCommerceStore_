5faeac59392acd1138ef8d0fb594001b3e6a1f43
111333444 <coban.fatmanur22@gmail.com>
2023-09-20 17:09:56 +0200

    FatmaNur registration kodu bitti

--- src/Main/ElifFatmanur.java
+++ src/Main/ElifFatmanur.java
@@ -13,31 +13,61 @@ public class ElifFatmanur extends BaseDriver {
 
     @Test
     public void navigateToRegistrationPage() {
+
         driver.get(" https://demo.nopcommerce.com/");
 
         //register butonunun tiklanabilir olma dogrulamasi
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='ico-register']")));
-        WebElement resgistrationLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
 
+        WebElement resgistrationLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
         resgistrationLink.click();
         MyFunc.Bekle(2);
 
         //kayit sayfasina erisim dogrulamasi
         Assert.assertTrue(driver.getCurrentUrl().toLowerCase().contains("register"));
 
-        WebElement dropdownElement1 = driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
-        Select dropdown1 = new Select(dropdownElement1);
-        dropdown1.selectByVisibleText("3");
+        //dogum tarihi secimi
+        WebElement dropdownElementDay = driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
+        WebElement dropdownElementMonth = driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"));
+        WebElement dropdownElementYear = driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"));
 
-        WebElement dropdownElement2 = driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"));
-        Select dropdown2 = new Select(dropdownElement2);
-        dropdown2.selectByVisibleText("October");
+        Select day = new Select(dropdownElementDay);
+        Select month = new Select(dropdownElementMonth);
+        Select year = new Select(dropdownElementYear);
 
-        WebElement dropdownElement3 = driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"));
-        Select dropdown3 = new Select(dropdownElement3);
-        dropdown3.selectByVisibleText("1990");
+        day.selectByVisibleText("3");
+        month.selectByVisibleText("October");
+        year.selectByVisibleText("1990");
 
         MyFunc.Bekle(3);
-        System.out.println();
+
+        //kullanici bilgileri
+        WebElement firstName = driver.findElement(By.id("FirstName"));
+        WebElement lastName = driver.findElement(By.id("LastName"));
+        WebElement eMail = driver.findElement(By.id("Email"));
+        WebElement password = driver.findElement(By.id("Password"));
+        WebElement confirmPass = driver.findElement(By.id("ConfirmPassword"));
+
+        firstName.sendKeys("Ayse");
+        lastName.sendKeys("deniz");
+        eMail.sendKeys("ayse0500@gmail.com");
+        password.sendKeys("ays.123");
+        confirmPass.sendKeys("ays.123");
+
+        WebElement rgstButton = driver.findElement(By.id("register-button"));
+        rgstButton.click();
+
+        //basarili kayit mesaji dogrulamasi
+        WebElement textSuccess = driver.findElement(By.cssSelector("div[class='result']"));
+        Assert.assertTrue(textSuccess.getText().equals("Your registration completed"));
+
+        WebElement cntButton = driver.findElement(By.cssSelector("a[class='button-1 register-continue-button']"));
+        cntButton.click();
+
+        //ana sayfaya yonlendirilme dogrulamasi
+        Assert.assertTrue(driver.getCurrentUrl().equalsIgnoreCase("https://demo.nopcommerce.com/"));
+
+
+
     }
     }
